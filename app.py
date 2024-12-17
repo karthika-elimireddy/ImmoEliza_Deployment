f7dad1d9d9a5da8788a48091c176197b018cd0da
Karthika_E <184744491+karthika-elimireddy@users.noreply.github.com>
2024-12-17 20:42:51 +0100

    update app

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ import category_encoders as ce
 import joblib
 from price_model import PriceModel
 from PIL import Image
+
 df = pd.read_csv("data/dataset.csv")
 df["locality"] = df["locality"].fillna("Unknown").astype(str)
-localities=["Select Locality"]
+localities = ["Select Locality"]
 localities.extend(sorted(df["locality"].str.lower().unique()))
-buildingstates = ["Select",
+buildingstates = [
+    "Select",
     "AS_NEW",
     "JUST_RENOVATED",
     "GOOD",
@@ -23,7 +25,7 @@ buildingstates = ["Select",
 
 # Mapping building states to numerical values
 state_to_value = {
-    "Select":0,
+    "Select": 0,
     "AS_NEW": 6,
     "JUST_RENOVATED": 5,
     "GOOD": 4,
@@ -33,7 +35,7 @@ state_to_value = {
 }
 years = list(range(2024, 1929, -1))
 # Initialize session state to track the page
-if 'page' not in st.session_state:
+if "page" not in st.session_state:
     st.session_state.page = 1
 # Streamlit UI
 image = Image.open("logo/logo.jpg")
@@ -41,29 +43,44 @@ resized_image = image.resize((100, 90))
 st.image(resized_image, use_container_width=False)
 
 # Change the color of the header
-st.markdown("<h2 style='color: orange;'>Immo Eliza Property Price Estimation</h2>", unsafe_allow_html=True)
+st.markdown(
+    "<h2 style='color: orange;'>Immo Eliza Property Price Estimation</h2>",
+    unsafe_allow_html=True,
+)
 
-if st.session_state.page == 1 :
-    st.markdown("<h2 style='color: black;'>Property Information</h2>",unsafe_allow_html=True)
-    st.markdown("<p style='color: black;'>Fields marked with * are mandatory.</p>",unsafe_allow_html=True)
+if st.session_state.page == 1:
+    st.markdown(
+        "<h2 style='color: black;'>Property Information</h2>", unsafe_allow_html=True
+    )
+    st.markdown(
+        "<p style='color: black;'>Fields marked with * are mandatory.</p>",
+        unsafe_allow_html=True,
+    )
     locality = st.selectbox("Select Locality *", options=localities)
     property_type = st.radio(
         "Property Type",
         options=[1, 0],
-        format_func=lambda x: "HOUSE" if x == 1 else "APARTMENT"
+        format_func=lambda x: "HOUSE" if x == 1 else "APARTMENT",
     )
-    next_button1 = st.button("Next",key="button1")
+    next_button1 = st.button("Next", key="button1")
     if next_button1:
-        if locality == 'Select Locality':
+        if locality == "Select Locality":
             st.error("Please select Locality.")
         else:
             st.session_state.locality = locality
             st.session_state.page = 2
-            
 
-if st.session_state.page == 2 :
-    st.markdown("<h2 style='color: black;'>Additional Information</h2>",unsafe_allow_html=True)
-    livingArea = st.number_input("Living Area (sqm) *", min_value=0.0, step=1.0,help="ℹ️ The living area refers to the part of a property that is habitable and used for day-to-day living. It is the portion of the home that is enclosed and usable for living purposes")
+
+if st.session_state.page == 2:
+    st.markdown(
+        "<h2 style='color: black;'>Additional Information</h2>", unsafe_allow_html=True
+    )
+    livingArea = st.number_input(
+        "Living Area (sqm) *",
+        min_value=0.0,
+        step=1.0,
+        help="ℹ️ The living area refers to the part of a property that is habitable and used for day-to-day living. It is the portion of the home that is enclosed and usable for living purposes",
+    )
     bedrooms = st.number_input("Number of Bedrooms *", min_value=0, step=1)
     bathrooms = st.number_input("Number of Bathrooms *", min_value=0, step=1)
     toilets = st.number_input("Number of Toilets *", min_value=0, step=1)
@@ -80,8 +97,8 @@ if st.session_state.page == 2 :
     buildingState = st.selectbox("Building Condition *", options=buildingstates)
     buildingState = state_to_value[buildingState]
     constructionYear = st.selectbox("Year of Construction", options=years)
-    next_button2 = st.button("Next",key="button2")
-    
+    next_button2 = st.button("Next", key="button2")
+
     if next_button2:
         if livingArea < 20:
             st.error("Please specify a valid living area (minimum 20sqm).")
@@ -104,26 +121,38 @@ if st.session_state.page == 2 :
             st.session_state.buildingState = buildingState
             st.session_state.constructionYear = constructionYear
             st.session_state.page = 3
-            
 
-if st.session_state.page == 3 :
-    st.markdown("<h2 style='color: black;'>Additional Information (Optional)</h2>",unsafe_allow_html=True)
-    facades = st.number_input("Number of Facades", min_value=0, step=1)
-    
-    mobib_score = st.number_input(
-        "Mobility Score ", min_value=0.0, max_value=10.0, step=0.1,help="ℹ️ Choose a mobib score number between 0,0 and 10,0."
+
+if st.session_state.page == 3:
+    st.markdown(
+        "<h2 style='color: black;'>Additional Information (Optional)</h2>",
+        unsafe_allow_html=True,
     )
-    
-    cadastralIncome = st.number_input("Cadastral Income", min_value=0.0, step=1.0,help="ℹ️ This income can include rent, leasing income, or any other financial benefit derived from the ownership or use of the property as determined by its cadastral assessment.")
-    estimate_button = st.button("Estimate Price",key="button3")
-    
+    facades = st.number_input("Number of Facades", min_value=0, step=1)
+
+    mobib_score = st.number_input(
+        "Mobility Score ",
+        min_value=0.0,
+        max_value=10.0,
+        step=0.1,
+        help="ℹ️ Choose a mobib score number between 0,0 and 10,0.",
+    )
+
+    cadastralIncome = st.number_input(
+        "Cadastral Income",
+        min_value=0.0,
+        step=1.0,
+        help="ℹ️ This income can include rent, leasing income, or any other financial benefit derived from the ownership or use of the property as determined by its cadastral assessment.",
+    )
+    estimate_button = st.button("Estimate Price", key="button3")
+
     # Prediction
     if estimate_button:
         st.session_state.facades = facades
         st.session_state.mobib_score = mobib_score
         st.session_state.cadastralIncome = cadastralIncome
         try:
-        # input data
+            # input data
             input_data = {
                 "locality": st.session_state.locality,
                 "mobib_score": st.session_state.mobib_score,
@@ -146,22 +175,22 @@ if st.session_state.page == 3 :
             # Predict price
             st.session_state.predicted_price = model.predict(preprocessed_data)
             # Display the result
-            #st.success(f"The estimated price of the property is: €{st.session_state.predicted_price:,.2f}")
-            
+            # st.success(f"The estimated price of the property is: €{st.session_state.predicted_price:,.2f}")
+
         except Exception as e:
             st.error(f"An error occurred: {e}")
         st.session_state.page = 4
-          
 
-if st.session_state.page == 4 :
-    st.markdown("<h2 style='color: black;'>Estimated Property Price</h2>",unsafe_allow_html=True)
-    st.success(f"The estimated price of the property is: €{st.session_state.predicted_price:,.2f}")
+
+if st.session_state.page == 4:
+    st.markdown(
+        "<h2 style='color: black;'>Estimated Property Price</h2>",
+        unsafe_allow_html=True,
+    )
+    st.success(
+        f"The estimated price of the property is: €{st.session_state.predicted_price:,.2f}"
+    )
     back_button = st.button("Estimate another Property", key="button4")
-    if back_button :
+    if back_button:
         st.session_state.clear()
         st.session_state.page = 1
-        
-        
-        
-    
-
